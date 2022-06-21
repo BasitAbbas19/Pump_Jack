0a096e4f4cb797e02da4f009e81984f4f7330060
Basit Abbas <abbasbasit723@gmail.com>
2022-06-21 13:48:47 +0500

    First commit

--- Slope_Formula_Task_Basit.js
+++ Slope_Formula_Task_Basit.js
@@ -3,5 +3,10 @@ const prompt=require("prompt-sync")({fake_val:"OPTIONAL CONFIG VALUES HERE",});
  let x2=parseInt(prompt("Enter the second xcoordinate:"));
  let y1=parseInt(prompt("Enter the first ycoordinate:"));
 let y2=parseInt(prompt("Enter the second ycoordinate:"));
-let gradient= (y2-y1)/(x2-x1)
-console.log('The slope is ',gradient );
+function slope_formula(x11,x22,y11,y22){
+    let gradient= (y2-y1)/(x2-x1);
+    return gradient;
+}
+let output=slope_formula(x1,x2,y1,y2);
+console.log('The slope of the given coordinates is ',output );
+
